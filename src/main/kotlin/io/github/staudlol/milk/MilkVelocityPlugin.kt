f3933e9a2bfe0a90b27b36916553fb8333cea552
Staud <77198963+staudlol@users.noreply.github.com>
2023-05-14 10:12:54 +0100

    small updates

--- src/main/kotlin/io/github/staudlol/milk/MilkVelocityPlugin.kt
+++ src/main/kotlin/io/github/staudlol/milk/MilkVelocityPlugin.kt
@@ -3,6 +3,7 @@ package io.github.staudlol.milk
 import com.google.inject.Inject
 import com.velocitypowered.api.command.CommandManager
 import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
 import com.velocitypowered.api.plugin.Plugin
 import com.velocitypowered.api.proxy.ProxyServer
 import io.github.staudlol.milk.command.AlertCommand
@@ -15,16 +16,14 @@ import java.util.logging.Logger
     description = "A proxy plugin for Velocity.",
     authors = ["Staud"]
 )
-class MilkSpigotPlugin @Inject constructor(
-
+class MilkVelocityPlugin @Inject constructor(
     private val server: ProxyServer,
     private val logger: Logger,
     private val commandManager: CommandManager
 ) {
 
     @Subscribe
-    fun onProxyInitialization()
-    {
+    fun onProxyInitialization(event: ProxyInitializeEvent) {
         logger.info("Milk has been initialized.")
 
         commandManager.register("alert", AlertCommand(server))
